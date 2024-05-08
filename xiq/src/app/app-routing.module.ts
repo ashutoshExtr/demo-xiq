0527b51aa1c99c43388f2e8bdf934967f358151e
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-05-08 11:15:00 +0530

    side nav done

--- xiq/src/app/app-routing.module.ts
+++ xiq/src/app/app-routing.module.ts
@@ -4,8 +4,17 @@ import { LoginComponent } from './login/login.component';
 import { XiqNavComponent } from './xiq-nav/xiq-nav.component';
 import { CopilotComponent } from './copilot/copilot.component';
 import { AuthGuard } from './auth.guard.service';
+import { ConfigureComponent } from './configure/configure.component';
+import { ManageComponent } from './manage/manage.component';
+import { MlInsightsComponent } from './ml-insights/ml-insights.component';
+import { EssentialsComponent } from './essentials/essentials.component';
+import { A3Component } from './a3/a3.component';
 
 const routes: Routes = [
+  { path: '', 
+    redirectTo: 'login', 
+    pathMatch: 'full'
+  },
   {
     path: 'login',
     component: LoginComponent
@@ -18,9 +27,30 @@ const routes: Routes = [
       // Add other paths here
     //]
   },
-  { path: '', 
-    redirectTo: 'login', 
-    pathMatch: 'full'
+  {
+    path: 'configure',
+    component: ConfigureComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'manage',
+    component: ManageComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'insights',
+    component: MlInsightsComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'essentials',
+    component: EssentialsComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'a3',
+    component: A3Component,
+    canActivate: [AuthGuard],
   },
 ];
 
