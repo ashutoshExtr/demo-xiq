4ab029c96b641a0d7a53301a4fbf97dee363d893
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-05-08 16:19:47 +0530

    WIP

--- xiq/src/app/xiq-nav/xiq-nav.component.ts
+++ xiq/src/app/xiq-nav/xiq-nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
+import { Authservice } from '../auth.service';
 
 @Component({
   selector: 'app-xiq-nav',
@@ -11,10 +12,10 @@ export class XiqNavComponent {
 
   @ViewChild('sidenav') sidenav!: MatSidenav;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, public authservice: Authservice) {}
 
-  navigateTo(route: string) {
-    this.router.navigate([route]);
-    this.sidenav.open();
-  }
+  // navigateTo(route: string) {
+  //   this.router.navigate([route]);
+  //   this.sidenav.open();
+  // }
 }
